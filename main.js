36ec9b20c5fd09364ff7878d676174f2ece2bb0e
fadfadhil <fadhilsyawaludin5@gmail.com>
2025-05-04 22:50:48 +0700

    learning javascript firs move my progres

---
+++ main.js
@@ -0,0 +1,7 @@
+function ubahwarna(){
+    const elemen = document.getElementById("teks1");
+    const elemen2 = document.getElementById("parag");
+
+    elemen.style.background = "red";
+    elemen2.textContent="hello brayyy!!!";
+}
