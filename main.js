cfdb1ce55a9fa09aa1a283fd59e4908836564ee3
fadfadhil <fadhilsyawaludin5@gmail.com>
2025-05-05 22:58:43 +0700

    Update main.js
    penambahan fungsi untuk tombol kedua

--- main.js
+++ main.js
@@ -5,3 +5,10 @@ function ubahwarna(){
     elemen.style.background = "red";
     elemen2.textContent="hello brayyy!!!";
 }
+function ubahwarna2(){
+    const elemen3 = document.getElementById("teks2");
+    const elemen4 = document.getElementById("parag2");
+
+    elemen3.style.background = "blue";
+    elemen4.textContent="selamat menikmati web saya!!!"
+}
